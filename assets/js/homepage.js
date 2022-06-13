737df6eb13c0211ab8b428d63563c043da1f4d59
osarumen misan <eosarumenwork@yahoo.com>
2022-06-13 11:45:04 -0400

    reposities with issues can now be looked up

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,87 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var reposSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault()
+  // get value from input element
+  
+  var username=nameInputEl.value.trim();
+
+  if(username){
+    getUserRepos(username);
+    nameInputEl.value = "";
+
+  } else {
+    alert("please enter a GitHub username");
+  }
+
+};
+var getUserRepos = function(user) {
+  // format the github api url
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) { 
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+      
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+    console.log(response);
+    
+  })
+  .catch(function(error){ 
+    //Notice this `.catch()` getting chained onto the end of the  `.then()`method
+    alert("Unable to connect to GitHub");
+  });
+};
+var displayRepos = function(repos, searchTerm){
+  //check if api returned any repos
+  if(repos.length === 0){
+
+  repoContainerEl.textContent = "No repositories found.";
+  return;
+  }
+  reposSearchTerm.textContent = searchTerm;
+// loop over repos
+for (var i = 0; i < repos.length; i++) {
+  // format repo name
+  var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+  // create a container for each repo
+  var repoEl = document.createElement("div");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+  // create a span element to hold repository name
+  var titleEl = document.createElement("span");
+  titleEl.textContent = repoName;
+
+  // append to container
+  repoEl.appendChild(titleEl);
+
+//create a status element
+var statusEl = document.createElement("span");
+statusEl.classList = "flex-row align-center";
+
+// check if current repo has issues or not
+if (repos[i].open_issues_count > 0) {
+  statusEl.innerHTML =
+    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+} else {
+  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+}
+
+// append to container
+repoEl.appendChild(statusEl);
+  // append container to the dom
+  repoContainerEl.appendChild(repoEl);
+}
 };
 
-
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
